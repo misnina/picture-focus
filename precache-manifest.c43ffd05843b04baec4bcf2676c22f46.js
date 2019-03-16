1cc6437bacd152c293ac6957653ae3cb9332e1e8
Nina <ninarachaebuie@gmail.com>
2019-03-15 22:52:40 -0500

    Updates

---
+++ precache-manifest.c43ffd05843b04baec4bcf2676c22f46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17c266af79dbf9fdb4c06cc0cd29457e",
+    "url": "/picture-focus/static/media/main-blurred.17c266af.png"
+  },
+  {
+    "revision": "2ae6da65763afdd100fb",
+    "url": "/picture-focus/static/js/runtime~main.f6dadbef.js"
+  },
+  {
+    "revision": "a147bb0d8918da119288",
+    "url": "/picture-focus/static/js/main.5665629a.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/picture-focus/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "a147bb0d8918da119288",
+    "url": "/picture-focus/static/css/main.4cad0046.chunk.css"
+  },
+  {
+    "revision": "1b499e3cb117d75eab710e6e874d5520",
+    "url": "/picture-focus/index.html"
+  }
+];
